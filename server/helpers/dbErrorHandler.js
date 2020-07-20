59bb52b65f8b890aca0163fce597e02cb36f66ca
vd89 <daxcode89@gmail.com>
2020-07-20 11:32:15 +0530

    error handler  for database

---
+++ server/helpers/dbErrorHandler.js
@@ -0,0 +1,38 @@
+/** @format */
+
+const getErrorMessage = (err) => {
+	let message = '';
+	if (err.code) {
+		switch (err.code) {
+			case 11000:
+			case 11001:
+				message = getUniqueErrorMessage(err);
+				break;
+			default:
+				message = 'Something went wrong';
+				break;
+		}
+	} else {
+		for (const errName in err.errors) {
+			if (err.errors[errName].messsage) {
+				message = err.errors[errName].message;
+			}
+		}
+	}
+	return message;
+};
+const getUniqueErrorMessage = (err) => {
+	let output;
+	try {
+		let fieldName = err.message.substring(
+			err.message.lastIndexOf('.$') + 2,
+			err.message.lastIndexOf('_1')
+		);
+		output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) + ' already exists';
+	} catch (ex) {
+		output = 'Unique field already exists';
+	}
+	return output;
+};
+
+export default { getErrorMessage };
